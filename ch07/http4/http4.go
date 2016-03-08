63590d287c67b306ba141cea35d6ba7281f37d4a
robert.kuhar <robert.kuhar@climate.com>
2016-03-07 23:13:26 -0800

    Progress through 7.7  - Copy-and-Paste programming.  Geez.

---
+++ ch07/http4/http4.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"fmt"
+)
+
+type dollars float32
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if (err != nil) {
+		log.Fatal(err)
+	}
+}
